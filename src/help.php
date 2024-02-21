dd620f0eae2d3254e9a92acec45b91ce41026f1b
lemher6-code <lemher6@gmail.com>
2024-02-21 16:26:13 -0600

    improving help page style.

--- src/help.php
+++ src/help.php
@@ -10,73 +10,83 @@
 ?>
 
   
-    <div class="content">
+    <div class="contentHelp">
 
       <div class="pageTitle">
         <h2>HELP</h2>
       </div>
+      <br>
 
-      <div class="leftBlock">
-        <br>
-        <h3>Guideline to Contribute to the CIS History Timeline</h3>
-        <br>
+      <div class="tl-guidelines">
+      <div class="content">
+          <h3>Guideline to Contribute to the CIS History Timeline</h3>
+          <br>
 
-        <b>This document provides a guideline on best practices while contributing to the CIS History Timeline (CIS-HT).</b>
+          <b>This document provides a guideline on best practices while contributing to the CIS History Timeline (CIS-HT).</b>
+          <br><br>
+          The purpose of the CIS-HT is to provide a portal where CIS students, researchers, and the public in general can find important past (and future, i.e. soon-to-be-past) 
+          events in the History of the CIS and CI in general.  Here, users will be able to upload pictures of attendees and participants in such CIS events; original documents,
+          manuscripts, and reports that reflect important milestones in our history and the history of the development of CI and AI in general; videos with interviews of 
+          prominent researchers; and so on.  The CIS-HT follows a wikipedia-like model where anyone can contribute. However, ours is a moderated model, meaning: contributions 
+          will be submitted and initially hidden from other users. All submissions will then be scrutinized by the CIS History Committee members who will approve or reject 
+          the contribution according to the following criteria:
+          <br><br>
+          <b>1.</b> Photos/Videos must be owned by the authors;
+          <br>
+          <b>2.</b> Permissions for use of the material must be obtained whenever applicable;
+          <br>
+          <b>3.</b> Materials/Events should not focus on ‘self-promotion’ or the promotion of any particular group, but of the CIS and its members in general -- e.g. 
+                    pictures of attendees in a conference must not focus on one person/group of people;
+          <br>          
+          <b>4.</b> Documents must have a recognizable and widely accepted historical value to the CIS;
+          <br>
+          ...
+          <br><br>
+          Once approved, the contribution will be made publicly visible and the author(s) will be notified. If any contribution violates any of the guidelines, the author of the contribution will be simply informed of the guideline number that was violated. No further explanations will be provided, however one opportunity for an appeal to the decision will be accepted.
+          <br><br>
+          Indicate in the submission form which criterion is ‘appeable’ -- e.g. #4 would not be appeable. The form should include a justification for the submission.
+          <br><br>
+          For sake of documenting the reason the committee denied a contribution and avoid having to list every single reason, the form will include a reason for the denial.  
+          <br><br>
+          Add information on how to submit (directly on the website or by email). 
+          <br><br>
+          Copyright material: should be mentioned? Should it list the name of the owner? Keep archive of all submission forms, emails, etc… with copyright information.
+          <br><br>
+          Make website look and feel like a museum 
+          <br><br>
+          Prepare the form for downloading and online access
+        </div>
         <br><br>
-        The purpose of the CIS-HT is to provide a portal where CIS students, researchers, and the public in general can find important past (and future, i.e. soon-to-be-past) 
-        events in the History of the CIS and CI in general.  Here, users will be able to upload pictures of attendees and participants in such CIS events; original documents,
-         manuscripts, and reports that reflect important milestones in our history and the history of the development of CI and AI in general; videos with interviews of 
-         prominent researchers; and so on.  The CIS-HT follows a wikipedia-like model where anyone can contribute. However, ours is a moderated model, meaning: contributions 
-         will be submitted and initially hidden from other users. All submissions will then be scrutinized by the CIS History Committee members who will approve or reject 
-         the contribution according to the following criteria:
-        <br><br>
-        <b>1.</b> Photos/Videos must be owned by the authors;
-        <br>
-        <b>2.</b> Permissions for use of the material must be obtained whenever applicable;
-        <br>
-        <b>3.</b> Materials/Events should not focus on ‘self-promotion’ or the promotion of any particular group, but of the CIS and its members in general -- e.g. 
-                  pictures of attendees in a conference must not focus on one person/group of people;
-        <br>          
-        <b>4.</b> Documents must have a recognizable and widely accepted historical value to the CIS;
-        <br>
-        ...
-        <br><br>
-        Once approved, the contribution will be made publicly visible and the author(s) will be notified. If any contribution violates any of the guidelines, the author of the contribution will be simply informed of the guideline number that was violated. No further explanations will be provided, however one opportunity for an appeal to the decision will be accepted.
-        <br><br>
-        Indicate in the submission form which criterion is ‘appeable’ -- e.g. #4 would not be appeable. The form should include a justification for the submission.
-        <br><br>
-        For sake of documenting the reason the committee denied a contribution and avoid having to list every single reason, the form will include a reason for the denial.  
-        <br><br>
-        Add information on how to submit (directly on the website or by email). 
-        <br><br>
-        Copyright material: should be mentioned? Should it list the name of the owner? Keep archive of all submission forms, emails, etc… with copyright information.
-        <br><br>
-        Make website look and feel like a museum 
-        <br><br>
-        Prepare the form for downloading and online access
+      </div>
 
-        <br><br><hr><br><br>
+      <br><br>
 
+      <div class="tl-howToNavegate">
+        <div class="content">
+          <span id="navegation"> </span>
+            <h2>How to Navigate the Timeline</h2>
+            <img class="helpImg" src="/img/Timeline-navegation.png" width="90%" title="How to navigate the Timeline.">
+        </div>
+      </div>
 
-        <span id="navegation"> </span>
-          <h2>How to Navigate the Timeline</h2>
-          <img class="helpImg" src="/img/Timeline-navegation.png" width="90%" title="How to navigate the Timeline.">
+      
+      <br><br><br><br>
 
-        <br><br><hr><br><br>
+      <div class="content">
 
         <span id="creating"> </span>
           <h2>How To Create a New Event</h2>
           <ul class="helpItem">
             <li>Click on the "Create Event" menu button.</li>
             <img class="helpImg" src="/img/CreateEvent.png" width="45%" title="Create Option">
-            <li>Field the event information <a href="#Enfio"><i style="font-size:24px; color:#20b2e2" class="fa">&#xf059;</i></a>. Every event must have at least a title and a start date.</li>
+            <li>Field the event information <a href="#Enfio"><button type="button" class="qButton">?</button></a>. Every event must have at least a title and a start date.</li>
             <li>Include the comments/reason about the new event at the end of form and click on the "Create" button to submit the form.</li>
-            <li>You can update the new event later after creating it. The new event will move to production when the Historical Committee approve it. </li>
+            <li>You can update the new event later after creating it. The new event will move to production when the Historical Committee approve it.</li>
           </ul>
+        
+          <br><br><hr><br><br>
 
-        <br><br><hr><br><br>
-
-
+        
           <span id="Enfio"> </span>
           <h2>The Event Information</h2>
           <ol class="helpItem">
@@ -107,10 +117,11 @@
             <li>Include the comments/reason about the new event at the end of form and click on the "Create" button to submit the form.</li>
               <img class="helpImg" src="/img/EventComments.png" width="80%" title="Event Media">
           </ol>
-
-
+        
+          
         <br><br><hr><br><br>
 
+        
         <span id="updating"> </span>
           <h2>How To Update an Event</h2>
           <ul class="helpItem">
@@ -123,8 +134,10 @@
               You can continue editing the event again until the Historical Committee approve or reject your request.</li>
               <li>You can see and edit your updates and removals with the menu option "Waiting Approval."</li>
           </ul>
-
+          
         <br><br><hr><br><br>
+
+      
 
         <span id="removing"> </span>
           <h2>How To Remove an Event</h2>
@@ -136,8 +149,8 @@
             <li>Include your comments or reason why the event should be removed for the committee. Click on "Yes" to request the removal.</li>
             <li>You can see and edit your updates and removals with the menu option "Waiting Approval."</li>
           </ul>
-        
-          <br><br><hr><br><br>
+
+        <br><br><hr><br><br>
 
         <span id="waiting"> </span>
           <h2>Review and Update your "Waiting for Approval" Requests</h2>
